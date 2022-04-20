b4d7bb017c17e00780b359daba524d53777adca8
Enzo <enzo.soulier@iut-rodez.fr>
2022-04-20 16:13:23 +0200

    Projet initial

---
+++ src/test/java/com/iut/rodez/Recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iut.rodez.Recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
