6fe15535716a482596fbb4f590ab87217b2f140f
Enzo <enzo.soulier@iut-rodez.fr>
2022-05-03 11:36:02 +0200

    CrossOrigin

--- src/main/java/com/iut/rodez/Recipes/controller/RecipeController.java
+++ src/main/java/com/iut/rodez/Recipes/controller/RecipeController.java
@@ -3,11 +3,13 @@ package com.iut.rodez.Recipes.controller;
 import com.iut.rodez.Recipes.model.Recipes;
 import com.iut.rodez.Recipes.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class RecipeController {
 
--- src/main/java/com/iut/rodez/Recipes/controller/UnitsController.java
+++ src/main/java/com/iut/rodez/Recipes/controller/UnitsController.java
@@ -3,11 +3,13 @@ package com.iut.rodez.Recipes.controller;
 import com.iut.rodez.Recipes.model.Units;
 import com.iut.rodez.Recipes.service.UnitsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UnitsController {
 
