220f78e233fca5615f5385d790653ab30c087f3b
Zhau <enzo.soulier@iut-rodez.fr>
2022-06-02 09:55:31 +0200

    New response entity for type

--- src/main/java/com/iut/rodez/Recipes/service/TypeService.java
+++ src/main/java/com/iut/rodez/Recipes/service/TypeService.java
@@ -2,7 +2,10 @@ package com.iut.rodez.Recipes.service;
 
 import com.iut.rodez.Recipes.model.Type;
 import com.iut.rodez.Recipes.repository.TypeRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,9 +17,9 @@ public class TypeService {
     @Autowired
     private TypeRepository typeRepository;
 
-    public List<Type> getTypesRecipe() {
+    public ResponseEntity<List<Type>> getTypesRecipe() {
         List<Type> typesRecipe = new ArrayList<>();
         typeRepository.findAll().forEach(typesRecipe::add);
-        return typesRecipe;
+        return new ResponseEntity<>(typesRecipe, HttpStatus.FOUND);
     }
 }
